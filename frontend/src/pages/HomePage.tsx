d700ae2b5f0f2bec0f5ed4352b70fa0e5f0e2259
deadyhu <rhu6@buffalo.edu>
2026-07-26 02:40:23 -0400

    Created the Navbar for homepage (testing tailwind and editing templates)

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,23 @@
 function HomePage() {
     return (
-        <div className="min-h-screen bg-gray-900 flex items-center justify-center">
-            <h1 className="text-4xl font-bold text-white">
-            This is the front end. React + Vite + TypeScript + Tailwind CSS all set up.
-            </h1>
+        <div>
+{/* -------------------------------------------NAV--------------------------------------------------------------------------------*/}
+            <nav className="bg-gray-800 text-white px-6 py-4 flex items-center shadow-md">
+                <h1 className = "font-bold text-xl tracking-wide text indig-400 mr-6">
+                    Personal Kanboard Project
+                </h1>
+
+                <div className = "flex items-center space-x-2">
+                    <a href = "#" className="bg-gray-900 px-4 py-2 rounded-md text-sm font-medium hover:bg-gray-700 transition">
+                        Home
+                    </a>
+
+                    <a href = "#" className = "px-4 py-2 rounded-md text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white transition">
+                        Something Else????
+                    </a>
+                </div>
+            </nav>
+{/* -------------------------------------------------------------------------------------------------------------------------------*/}
         </div>
     );
 }
