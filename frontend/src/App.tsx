e61fd98ef5c66b119e38b81cdc139e1226716c7e
ezfruit <ezhang8@buffalo.edu>
2026-07-25 22:45:52 -0400

    Added a basic login page at the route /login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router"
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
+
 function App() {
 
   return (
-    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">
-        This is the front end. React + Vite + TypeScript + Tailwind CSS all set up.
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
